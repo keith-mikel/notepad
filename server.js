96aeaa2ec970e463eb5699a2a4c128252195eda2
keith-mikel <keith.mikel@e5sol.com>
2023-07-24 12:07:05 -0500

    changed public

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
